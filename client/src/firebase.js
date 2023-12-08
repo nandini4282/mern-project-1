70ab48b42f8863c95b4c3778c92911dcbde9648c
nandini4282 <nandinityagi4282@gmail.com>
2023-12-08 12:49:14 +0530

    Second Commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyD0_18weDPsVK37p5ehFpKrw5t0vemv1Nw",
+  authDomain: "realto-d2604.firebaseapp.com",
+  projectId: "realto-d2604",
+  storageBucket: "realto-d2604.appspot.com",
+  messagingSenderId: "981725792907",
+  appId: "1:981725792907:web:25c21e7a2e15f7aafb628e"
 };
 
 // Initialize Firebase
